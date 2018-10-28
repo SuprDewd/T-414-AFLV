f76c29653c36023825491a981a542c188410258c
William Granados <william.granadosmatamoros@mail.utoronto.ca>
2018-10-27 20:58:08 -0400

    clearly incorporated attribution to CC in slides

---
+++ 01_introduction/update.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+pdflatex -synctex=1 -shell-escape -interaction=nonstopmode aflv_01_introduction.tex
---
+++ 04_problem_solving_paradigms/update.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+pdflatex -synctex=1 -shell-escape -interaction=nonstopmode aflv_04_problem_solving_paradigms.tex 
